50192b6818f80d6469abdc387fedc5dcebf187f0
arliss mcgugin <arlissgithub@gmail.com>
2023-11-02 23:20:11 -0500

    .

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
 func main() {
@@ -17,34 +16,18 @@ func main() {
 	}
 
 	// URLs are now stored in the 'urls' variable
-	fmt.Println("Number of URLs read from the file: ", len(urls))
-	
-	// url channels
-	urlsChan := make(chan string, len(urls))
-	resultsChan := make(chan bool, len(urls))
-	startTime := time.Now()
+		fmt.Println("Number of URLs read from the file: ", len(urls))
 
-	for i := 0; i < 3; i++ {
-		go worker(urlsChan, resultsChan, time.Now())
-	}
+		for _, url := range urls {
+			resp, err := http.Get(url)
+	        if err != nil {
+	            log.Fatalf("Error fetching URL %q: %v\n", url, err)
+	        }
+			defer resp.Body.Close()
 
-	for _, url := range urls {
-		urlsChan <- url
-	}
-	close(urlsChan)
-
-	successCount := 0
-	for range urls {
-		if success := <-resultsChan; success {
-			successCount++
+	        statusCode := resp.StatusCode
+			fmt.Println(url, " - ", statusCode)
 		}
-		
-	}
-	successPercentage := float64(successCount) / float64(len(urls)) * 100
-	fmt.Printf("%.2f%% of the URLs were successfully pinged.\n", successPercentage)
-
-	totalTime := time.Since(startTime)
-	fmt.Printf("Total time taken to ping URLs: %s\n", totalTime)
 }
 
 
@@ -77,34 +60,3 @@ func importUrl(filename string) ([]string, error) {
 	return urls, nil
 }
 
-// pingUrl tries to retrieve the URL using a HEAD request and returns the HTTP status code and error if any.
-func pingUrl(url string) (int, error) {
-	
-	client := http.Client{
-		Timeout: 10 * time.Second,
-	}
-	// We use a HEAD request to minimize data transfer.
-	resp, err := client.Head(url)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	return resp.StatusCode, nil
-}
-
-// worker pings URLs received on the urlsChan and sends the results on the resultsChan.
-func worker(urlsChan <-chan string, resultsChan chan<- bool, startTime time.Time) {
-	for url := range urlsChan {
-		// Ping the URL and send the result on the resultsChan.
-		status, err := pingUrl(url)
-		
-		success := err == nil && status == http.StatusOK
-		
-		resultsChan <- success
-
-		// Log the % of URLs that have been pinged.
-		elapsed := time.Since(startTime)
-		fmt.Printf("Pinged %s | Success: %t | Status Code: %d | Elapsed Time: %s\n", url, success, status, elapsed)
-	}
-}
